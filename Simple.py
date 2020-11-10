47c5dcd92471bbd9534b733edf9326eb04621620
juosorioca <61954072+juosorioca@users.noreply.github.com>
2020-11-09 20:56:29 -0500

    Hacer de l un parametro no constante

--- Simple.py
+++ Simple.py
@@ -10,10 +10,10 @@ from vpython import*
 # =============================================================================
 # Configuración inicial
 # =============================================================================
-display(title='Pendulos Acoplados simples', width=600, height=600,
+display(title='Pendulos Acoplados simples', width=600, height=400,
         background=color.black)
 
-graph=graph(width=600,height=400,title='<b>Pendulos Acoplados simples</b>',
+graph=graph(width=645,height=400,title='<b>Pendulos Acoplados simples</b>',
             xtitle='<i>Tiempo (s)</i>',ytitle='<i>Amplitud (m)</i>',
             foreground=color.black, background=color.black)
 recorrido = gcurve(graph=graph, color=color.orange) #Traza el recorrido deseado
@@ -21,31 +21,28 @@ recorrido = gcurve(graph=graph, color=color.orange) #Traza el recorrido deseado
 # =============================================================================
 # condiciones iniciales
 # =============================================================================
+theta = pi/3 # Angulo inicial
 g = 9.784 #gravedad 
 t=0 # tiempo inicial
 dt = 0.001 #diferencial temporal
-pivot=vector(0,20,0) #vector eje
-omega = 0.8 # vel angular
-
+pivot=vector(0,0,-10) #vector eje / z -> acerca la imagen
+omega = 0.25 # vel angular inicial
+l = 15 # longitud como parametro
+w = -g/l # frecuencia natural
 
 # =============================================================================
 # Sistema referencia cuerda-esfera
 # =============================================================================
-esfera=sphere(pos=vector(5,2,-10),radius=0.55,color=color.white,make_trail = True, 
-              trail_type = 'points',trail_color = color.purple, 
-              interval=25, retain=50)
+esfera=sphere(pos=vector(l*sin(theta), pivot.y - l*cos(theta), -10),radius=0.55,color=color.white,make_trail = True, 
+              trail_type = 'points',trail_color = color.purple, interval=25, retain=50)
 
 techo=box(pos=pivot, size=vector(0.5,0.5,0.5), color=color.red)
-cuerda=cylinder(pos=pivot, axis=esfera.pos - pivot, radius=0.05,color=color.white)
+cuerda=cylinder(pos=pivot, axis=esfera.pos - pivot, radius=0.05, color=color.white)
 
-l = mag(esfera.pos - pivot)
+
 # =============================================================================
 # movimiento
 # =============================================================================
-w = -g/l # frecuancia natural
-adyacente = (pivot.y - esfera.pos.y)/l 
-theta = acos(adyacente) #angulo mov.
-
 
 while t < 104: 
   rate(1500) # Homogeneidad de computo
