67d40a45a510bde8331b37e9f1821dd84394fdbd
Carolina-Valenzuela <73853351+Carolina-Valenzuela@users.noreply.github.com>
2020-11-17 16:45:15 -0500

    Update Péndulo simple forzado.py

--- Péndulo simple forzado.py
+++ Péndulo simple forzado.py
@@ -1,42 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 10 01:31:11 2020
+Created on Sat Nov 14 21:04:06 2020
 
-@author: Carolina Valenzuela
+@author: carov
 """
 
 from vpython import *
-display(title="Pendulo simple", width=600, height=600, background=color.black)
+display(title="Pendulo forzado", width=600, height=600, background=color.black)
+
+graph=graph(width=600,height=400,title='<b>Pendulo forzado</b>',
+            xtitle='<i>Tiempo (s)</i>',ytitle='<i>Amplitud (m)</i>',
+            foreground=color.black, background=color.black)
+recorrido = gcurve(graph=graph, color=color.orange)
 
 #Condiciones iniciales
-t=0
+omega_zero=0
+theta_zero=pi/4
 dt=0.001
+tmax=104
 g=9.784
-pivot=vector(0,15,0)
-omega=0
+l=10
+natural=-g/l
 
-Omega=0.02
-A=1
-F=A*cos(Omega*t) #Forzamiento
-k=1 #Amortiguamiento
 
-#Elementos
-esfera=sphere(pos=vector(5,0,0), radius=0.5, color=color.blue)
-techo=box(pos=pivot, size=vector(0.5,0.5,0.5), color=color.green)
+pivot=vector(0,0,-10)
+esfera=sphere(pos=vector(l*sin(theta_zero),pivot.y-l*cos(theta_zero),-10), radius=0.55, color=color.white)
+techo=box(pos=pivot, size=vector(0.5,0.5,0.5), color=color.red)
 cuerda=cylinder(pos=pivot, axis=esfera.pos-pivot, radius=0.05, color=color.white)
 
-l=mag(esfera.pos-pivot) #Longitud de la cuerda
-
-w = -g/l #Frecuencia natural
-theta = acos((pivot.y - esfera.pos.y)/l) #Angulo mov.
-
-while t < 104 and omega!=Omega: 
+t=0
+Omega=0.02
+f=1
+F=f*cos(Omega*t) #Forzamiento
+gamma=0.5
+k=2*gamma #Amortiguamiento
+while t < tmax and omega_zero!=Omega: 
   rate(1500) # Homogeneidad de computo
       
-  theta+=(omega*dt)
-  alpha =(w*sin(theta))-(k*omega)+F # θ'' + kθ' + g/l sen(θ) = F
-  omega+=(alpha*dt)
+  theta_zero+=(omega_zero*dt)
+  alpha=(natural*sin(theta_zero))-(k*omega_zero)+F # θ'' + kθ' + g/l sen(θ) = F
+  omega_zero+=(alpha*dt)
   
-  esfera.pos = vector(l*sin(theta), pivot.y-l*cos(theta), 0) #Posicion nueva
-  cuerda.axis = esfera.pos - pivot # Extremo nuevo
+  esfera.pos = vector(l*sin(theta_zero),pivot.y-l*cos(theta_zero),-10) #Posicion nueva
+  cuerda.axis = esfera.pos - cuerda.pos # Extremo nuevo
   t+=dt 
+  
+  recorrido.plot((t, theta_zero))
