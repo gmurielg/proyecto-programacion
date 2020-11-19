495d706af1756ad3e0bb74c41e980a5a6758f0a7
FelosConatus <feospinas@unal.edu.co>
2020-11-19 08:55:27 -0500

    Update Pendulo_ODE_approach.py

--- Pendulo_ODE_approach.py
+++ Pendulo_ODE_approach.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt #Test purpouses
+import matplotlib.pyplot as plt #Testeo
 from PIL import ImageDraw,Image #Modificacion y creacion de imagen (respectivamente)
 
 #//////////////////#
@@ -38,11 +38,11 @@ def pendulo(Ancho,Alto,L,theta):
 En esta ocasion el problema se abrodara a partir del calculo de la trayectoria del pendulo.
 A partir de esta se crearan suficientes imagenes por seguundo para obtener un video de la evolucion del sistema.
 Para ello se utilizara un diferencial de tiempo que simulara un tiempo continuo.
--Ecuacion diferencial que modela el sistema: \ddot\theta=-(g/l)sin(\theta) 
--Condiciones inicales \theta_0 y \dot\theta_0
+-Ecuacion diferencial que modela el sistema: ddtheta=-(g/l)sin(theta(t)) 
+-Condiciones inicales \theta_0 y dtheta_0
 Haciendo uso de la aproximacion: f(x)=f(x+dx)+dx(\dotf(x)):
-\theta(t+dt)=\theta(t)+dt(\dot\theta(t))
-\dot\theta(t+dt)=dot\theta(t)-(g/l)sin(\theta)
+theta(t+dt)=theta(t)+dt(\dtheta(t))
+dtheta(t+dt)=dtheta(t)-(g/l)sin(theta(t))
 """
 #Calculo del estado del sistema en n instantes (steps) de tiempo
 
@@ -57,7 +57,7 @@ def trayectoria(theta0,dtheta0,steps,dt,L,g=9.8):
     
     return estados 
 
-#a=trayectoria(np.pi/3,0,10000,0.001,10)
+a=trayectoria(np.pi/3,0,10000,0.001,10)
 #plt.plot(a[:,0])
 #plt.show()
 
@@ -66,26 +66,22 @@ Cuantos diferenciales de tiempo tengo en un segundo? 1/dt
 Si quiero mostras n frames por segundo, debo tomar un frame cada (1/dt)/n
 """
 
-def video(estados,fps,dt,l):
+def video(save_path,nombre,duracion,estados,fps,dt,l):
     frames=[]
     a=estados.shape[0]
     for i in range(a):
-        if i%(1/(dt*fps))==0:
+        if i%(int(1/(dt*fps)))==0:
             theta=estados[i,0]
             img=pendulo(400,400,l,theta)
             frames.append(img)
     
-    frames[0].save("C:/Users/janus/Desktop/pendulo_simple.gif",save_all=True,append_images=frames[1:],duration=60,loop=0)
-    
+    frames[0].save(save_path + nombre +".gif",save_all=True,append_images=frames[1:],duration=int(duracion),loop=0)
 
 
 #///////////////////////#
 #  PENDULO AMORTIGUADO  #
 #///////////////////////#
 """
-En esta ocasion el problema se abrodara a partir del calculo de la trayectoria del pendulo.
-A partir de esta se crearan suficientes imagenes por seguundo para obtener un video de la evolucion del sistema.
-Para ello se utilizara un diferencial de tiempo que simulara un tiempo continuo.
 -Ecuacion diferencial que modela el sistema: ddtheta(t)=-(g/l)sin(theta(t))-\gamma*dtheta(t)
 -Condiciones inicales \theta_0 y \dot\theta_0
 Haciendo uso de la aproximacion: f(x)=f(x+dx)+dx(\dotf(x)):
@@ -104,12 +100,27 @@ def trayectoria2(theta0,dtheta0,damping,steps,dt,L,g=9.8):
     return estados 
 
 
-#video(trayectoria(np.pi/3,0,10000,0.001,100),20,0.001,100)
-video(trayectoria2(np.pi/3,0,0.05,10000,0.001,50),20,0.001,50)
+#/////////////////////////////////#
+#  PENDULO AMORTIGUADO Y FORZADO  #
+#/////////////////////////////////#
+"""
+-Ecuacion diferencial que modela el sistema: ddtheta(t)=-(g/l)sin(theta(t))-\gamma*dtheta(t)
+-Condiciones inicales \theta_0 y \dot\theta_0
+Haciendo uso de la aproximacion: f(x)=f(x+dx)+dx(\dotf(x)):
+theta(t+dt)=theta(t)+dt*(dtheta(t))
+dtheta(t+dt)=dtheta(t)-dt*(g/l)sin(theta(t))-gamma*dtheta(t)+(F/m)cos(Wt)
+"""
+def trayectoria3(theta0,dtheta0,damping,f,W,steps,dt,L,g=9.8):
+    estados=np.zeros((steps,2))
+    estados[0,:]=np.array([theta0,dtheta0]) #Estado inicial
+    #Ahora el i-esimo elemento de estados correspondera al estado tras el i-esimo step
+    for i in range(steps-1):
+        ddtheta=-g/L*(np.sin(estados[i,0]))-damping*(estados[i,1])+f*np.cos(W*t)
+        estados[i+1,0]=estados[i,0]+dt*estados[i,1] #Nuevo angulo
+        estados[i+1,1]=estados[i,1]+dt*ddtheta #Nueva velocidad angular
+    
+    return estados 
 
-"""
-Ecuacion sin aproxiamcion de pequeñas oscilaciones <---Primera opcion
-Ecuacion con aproximacion de pequeñas oscialciones (Usar un resorte?)
-Mas razonable usar este approach para el pendulo doble y los pendulos acoplados
-pues sacamos el potencial de los sitemas de EDO
-"""
+#video("C:/Users/janus/Desktop/","PenduloSimple",90,trayectoria(np.pi/3,0,100000,0.001,5),30,0.001,100)
+#video("C:/Users/janus/Desktop/","PenduloAmortiguado",90,trayectoria2(np.pi/3,0,0.05,100000,0.001,5),30,0.001,100)
+#video("C:/Users/janus/Desktop/",PenduloForzado,trayectoria3(np.pi/3,0,0.05,10000,0.001,50),20,0.001,50)
