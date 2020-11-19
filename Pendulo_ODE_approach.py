82ae9425843baec61b4c655212b082f7f6aaf854
FelosConatus <feospinas@unal.edu.co>
2020-11-18 19:29:44 -0500

    Update Pendulo_ODE_approach.py

--- Pendulo_ODE_approach.py
+++ Pendulo_ODE_approach.py
@@ -72,16 +72,40 @@ def video(estados,fps,dt,l):
     for i in range(a):
         if i%(1/(dt*fps))==0:
             theta=estados[i,0]
-            img=pendulo(250,250,l,theta)
+            img=pendulo(400,400,l,theta)
             frames.append(img)
     
-    frames[0].save("C:/Users/janus/Desktop/pendulo_simple.gif",save_all=True,append_images=frames[1:],duration=40,laoop=0)
+    frames[0].save("C:/Users/janus/Desktop/pendulo_simple.gif",save_all=True,append_images=frames[1:],duration=60,loop=0)
     
-video(trayectoria(np.pi/3,0,10000,0.001,15),20,0.001,50)
+
 
 #///////////////////////#
 #  PENDULO AMORTIGUADO  #
 #///////////////////////#
+"""
+En esta ocasion el problema se abrodara a partir del calculo de la trayectoria del pendulo.
+A partir de esta se crearan suficientes imagenes por seguundo para obtener un video de la evolucion del sistema.
+Para ello se utilizara un diferencial de tiempo que simulara un tiempo continuo.
+-Ecuacion diferencial que modela el sistema: ddtheta(t)=-(g/l)sin(theta(t))-\gamma*dtheta(t)
+-Condiciones inicales \theta_0 y \dot\theta_0
+Haciendo uso de la aproximacion: f(x)=f(x+dx)+dx(\dotf(x)):
+theta(t+dt)=theta(t)+dt*(dtheta(t))
+dtheta(t+dt)=dtheta(t)-dt*(g/l)sin(theta(t))-gamma*dtheta
+"""
+def trayectoria2(theta0,dtheta0,damping,steps,dt,L,g=9.8):
+    estados=np.zeros((steps,2))
+    estados[0,:]=np.array([theta0,dtheta0]) #Estado inicial
+    #Ahora el i-esimo elemento de estados correspondera al estado tras el i-esimo step
+    for i in range(steps-1):
+        ddtheta=-g/L*(np.sin(estados[i,0]))-damping*(estados[i,1])
+        estados[i+1,0]=estados[i,0]+dt*estados[i,1] #Nuevo angulo
+        estados[i+1,1]=estados[i,1]+dt*ddtheta #Nueva velocidad angular
+    
+    return estados 
+
+
+#video(trayectoria(np.pi/3,0,10000,0.001,100),20,0.001,100)
+video(trayectoria2(np.pi/3,0,0.05,10000,0.001,50),20,0.001,50)
 
 """
 Ecuacion sin aproxiamcion de pequeñas oscilaciones <---Primera opcion
